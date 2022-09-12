904144f2d607f16c73531711c535e5b66620a581
lufebadeca <lufebadeca@gmail.com>
2022-09-12 17:56:26 -0500

    Mocile menu added and media queries adjusted

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 emailMenu.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click',toggleMobileMenu);
 
 console.log("JS added");
 
 function toggleDesktopMenu(){
     //desktopMenu.classList.add('active');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    console.log('function for mobile menu');
+    mobileMenu.classList.toggle('inactive');
 }
